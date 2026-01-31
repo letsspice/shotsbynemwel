648f9be911256d37b7145a54f3dd7ec569439242
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2026-01-31 18:23:54 +0300

    Update included:   - Noticed an issue with not setting the aspect ration correctly and fixed it   - Added the import for the About me section of the page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/app/components/sections/Hero";
+import About from "./components/sections/About";
 
 export default function HomePage() {
   return (
     <>
       <Hero />
+      <About />
       {/* Other sections will follow here */}
     </>
   );
