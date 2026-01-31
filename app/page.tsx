39a072ffd56278cb360b4a051824a379946cd7aa
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2026-01-31 22:04:50 +0300

    Update included:   - Added the mountain expeditions to the main entry point of the app to load it to the app's flow   - Create and style the mountain expeditions section loading the data from the data file keeping things very neat and organised

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/app/components/sections/Hero";
 import About from "./components/sections/About";
+import MountainExpeditions from "./components/sections/MountainExpeditions";
 
 export default function HomePage() {
   return (
     <>
       <Hero />
       <About />
+      <MountainExpeditions />
       {/* Other sections will follow here */}
     </>
   );
