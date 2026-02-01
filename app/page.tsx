220acf466b685b611348ceb102fe6e3426cf37ef
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2026-02-01 17:01:23 +0300

    Update included:   - Added the images that are supposed to be in the TrailNotes section of the website   - Add the Trailnotes component to the main entry point of the website

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Hero from "@/app/components/sections/Hero";
 import About from "./components/sections/About";
 import MountainExpeditions from "./components/sections/MountainExpeditions";
 import GallerySection from "./components/sections/GallerySection";
+import TrailStories from "./components/sections/TrailStories";
 
 export default function HomePage() {
   return (
@@ -10,6 +11,7 @@ export default function HomePage() {
       <About />
       <MountainExpeditions />
       <GallerySection />
+      <TrailStories />
       {/* Other sections will follow here */}
     </>
   );
